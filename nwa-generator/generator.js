c47811c535011f5e940725b75f6b6a3f2317c2aa
Neva <nevanjithdes@gmail.com>
2024-04-05 15:54:49 +0530

    Changed post to Heroku approved format.

--- nwa-generator/generator.js
+++ nwa-generator/generator.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-const API_ENDPOINT = "http://localhost:3000/api/weather/data/add";
+const API_ENDPOINT = "http://www.slweather.site/api/weather/data/add";
 
 function generateData(c) {
   const temperature = (Math.random() * (40 - 20) + 20).toFixed(2);
@@ -19,21 +19,56 @@ async function sendData() {
   try {
     var c = 1;
     var token =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlcyI6WyJhZG1pbiIsImVkaXRvciIsInZpZXdlciJdLCJpYXQiOjE3MTIyODY2NTgsImV4cCI6MTcxMjI5MDI1OH0.h2LOp0lRvSi40EssPvbLMw9g7KD_pL_bJMUqZDi4HW8";
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlcyI6WyJhZG1pbiIsImVkaXRvciIsInZpZXdlciJdLCJpYXQiOjE3MTIzMTE1MTMsImV4cCI6MTcxMjMxNTExM30.QKJvi2wHopPiiE39OZiRw9bt8Eop5-MqJSjuQCeAsPY";
 
-    do {
+    const requests = [];
+    while (c <= 25) {
       const data = generateData(c);
-      const response = await axios.post(API_ENDPOINT, data, {
-        headers: {
-          "x-auth-token": token,
-        },
-      });
-      console.log("Data sent successfully:", data);
+      const requestPromise = axios
+        .post(API_ENDPOINT, data, {
+          headers: {
+            "x-auth-token": token,
+          },
+        })
+        .then(() => {
+          console.log("Data sent successfully:", data);
+        })
+        .catch((error) => {
+          console.error("Failed to send data:", error.message);
+        });
+      requests.push(requestPromise);
       c++;
-    } while (c <= 25);
+    }
+
+    await Promise.all(requests);
+    console.log("All 25 requests sent successfully.");
+
+    setInterval(async () => {
+      c = 1;
+      const intervalRequests = [];
+      while (c <= 25) {
+        const data = generateData(c);
+        const requestPromise = axios
+          .post(API_ENDPOINT, data, {
+            headers: {
+              "x-auth-token": token,
+            },
+          })
+          .then(() => {
+            console.log("Data sent successfully:", data);
+          })
+          .catch((error) => {
+            console.error("Failed to send data:", error.message);
+          });
+        intervalRequests.push(requestPromise);
+        c++;
+      }
+      await Promise.all(intervalRequests);
+      console.log("25 requests sent successfully in 5-minute interval.");
+    }, 5 * 60 * 1000);
   } catch (error) {
     console.error("Failed to send data:", error.message);
   }
 }
 
-setInterval(sendData, 1 * 5 * 1000);
+sendData();
